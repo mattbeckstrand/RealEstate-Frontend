870e74cd9d85ae41722431715b56481cf3e5ba95
Matt Beckstrand <matt.beckstrand@gmail.com>
2025-02-13 13:14:33 -0700

    created own upload functionality

--- src/app/analysis/page.tsx
+++ src/app/analysis/page.tsx
@@ -1,9 +1,23 @@
+"use client"
 import { FileHandler } from '@/components/FileHandler'
+import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { Separator } from "@/components/ui/separator"
 
 export default function AnalysisPage() {
   return (
-    <div>
-      <FileHandler />
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <h1 className="text-2xl font-bold">Analysis</h1>
+          <FileHandler />
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
